2f4b9647b51a6658397e26174cf3648561c5a31d
Juan Pablo Orsay <jporsay@gmail.com>
2012-06-03 21:12:09 -0300

    added test code

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "event/GenericHandler.h"
 #include "app/State.h"
 #include "app/Settings.h"
+#include "manager/Shader.h"
+#include "gl/util/Helper.h"
 
 #include "parser/ThreeDS.h"
 #include "model/Model.h"
@@ -13,6 +15,8 @@
 SDL_Surface* screen;
 event::Manager* eventManager;
 bool running = true;
+
+model::Model* o;
 
 bool initSDL() {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
@@ -28,40 +32,69 @@ bool initSDL() {
 	return true;
 }
 
-bool initEverything() {
-	if (!initSDL()) {
-		return false;
-	}
-
+bool initGlew() {
 	GLenum glew_init;
 	glew_init = glewInit();
 	if (glew_init != GLEW_OK) {
 		std::cout << "Error initializing GLEW" << std::endl;
 		return false;
 	}
-	if (!GLEW_VERSION_2_1) {
-		std::cout << "OpenGL 2.1 not supported" << std::endl;
+	if (!GLEW_VERSION_3_3) {
+		std::cout << "OpenGL 3.3 not supported" << std::endl;
 		return false;
 	}
-	game::Settings::inst()->setResourcePath("resources/");
-	game::Settings::inst()->setShaderPath("resources/shaders/");
-	game::Settings::inst()->setModelPath("resources/models/");
+	return true;
+}
+
+bool initOpenGL() {
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+	return true;
+}
+
+bool initEverything() {
+	if (!initSDL() || !initGlew() || !initOpenGL()) {
+		return false;
+	}
+	app::Settings::inst()->setResourcePath("resources/");
+	app::Settings::inst()->setShaderPath("resources/shaders/");
+	app::Settings::inst()->setModelPath("resources/models/");
 	Logger::inst()->setFileName("log.txt");
-	eventManager = new event::Manager();
+	eventManager = new event::EventManager();
 	eventManager->registerHandler(new handler::GenericHandler());
+	return true;
+}
+
+bool loadEverything() {
+	parser::ThreeDS* parser = new parser::ThreeDS();
+	parser->setFile("Cube.3ds");
+	o = parser->parse();
+	o->upload();
+	manager::Shader::inst()->setPath(app::Settings::inst()->getShaderPath());
+	if (!manager::Shader::inst()->loadShaders()) {
+		return false;
+	}
+	if(!manager::Shader::inst()->compile("simple")) {
+		return false;
+	}
+	gl::ShaderProgram* simpleShader = manager::Shader::inst()->getProgram("simple");
+	simpleShader->bindAttribLocation("vert", gl::util::GL_ATTRIB_VERTEX);
+	simpleShader->link();
 	return true;
 }
 
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+	glClearColor(0.827, 0.827, 0.827, 1.0);
+	o->draw();
 	SDL_GL_SwapBuffers();
 }
 
 void run() {
-	game::State::inst()->setRunning(true);
-	while (game::State::inst()->isRunning()) {
-		if (!game::State::inst()->isHidden()) {
+	app::State::inst()->setRunning(true);
+	while (app::State::inst()->isRunning()) {
+		if (!app::State::inst()->isHidden()) {
 			eventManager->handleEvents();
 			render();
 		}
@@ -76,9 +109,10 @@ int main(int argc, char* argv[]) {
 	if (!initEverything()) {
 		return 1;
 	}
-	parser::ThreeDS* parser = new parser::ThreeDS();
-	parser->setFile("Box.3DS");
-	object::Object* o = parser->parse();
+	LOG_INFO("Starting demo");
+	if (!loadEverything()) {
+		return 1;
+	}
 	run();
 	clean();
 	return 0;
