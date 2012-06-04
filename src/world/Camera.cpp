e26361056fb6c3ca3a44717e67fca476dbdc581f
Juan Pablo Orsay <jporsay@gmail.com>
2012-06-04 02:27:51 -0300

    added camera

---
+++ src/world/Camera.cpp
@@ -0,0 +1,40 @@
+#include "Camera.h"
+#include "../app/GraphicSettings.h"
+namespace world {
+
+Camera::Camera() {
+	this->up = glm::vec3(0.0, 1.0, 0.0);
+	this->position = glm::vec3(0.0, 0.0, 4.0);
+	this->lookAt = glm::vec3(0.0);
+}
+
+Camera::Camera(glm::vec3 position) {
+	this->up = glm::vec3(0.0, 1.0, 0.0);
+	this->position = position;
+	this->lookAt = glm::vec3(0.0);
+}
+
+Camera::Camera(glm::vec3 position, glm::vec3 lookDirection) {
+	this->up = glm::vec3(0.0, 1.0, 0.0);
+	this->position = position;
+	this->lookAt = lookDirection;
+}
+Camera::Camera(glm::vec3 position, glm::vec3 lookDirection, glm::vec3 upDirection) {
+	this->position = position;
+	this->lookAt = lookDirection;
+	this->up = upDirection;
+}
+
+glm::mat4 Camera::worldToCamera() {
+	return glm::lookAt(this->position, this->lookAt, this->up);
+}
+
+glm::mat4 Camera::toScreen() {
+	return glm::perspective(45.0f, 1.0f*app::GraphicSettings::screenWidth/app::GraphicSettings::screenHeight, 0.1f, 100.0f);
+}
+
+Camera::~Camera() {
+
+}
+
+}
---
+++ src/world/Camera.h
@@ -0,0 +1,26 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include "../glm/glm.hpp"
+#include "../glm/gtc/type_ptr.hpp"
+#include "../glm/gtc/matrix_transform.hpp"
+
+namespace world {
+
+class Camera {
+protected:
+	glm::vec3 lookAt;
+	glm::vec3 position;
+	glm::vec3 up;
+public:
+	Camera();
+	Camera(glm::vec3 position);
+	Camera(glm::vec3 position, glm::vec3 lookDirection);
+	Camera(glm::vec3 position, glm::vec3 lookDirection, glm::vec3 upDirection);
+	glm::mat4 worldToCamera();
+	glm::mat4 toScreen();
+	virtual ~Camera();
+};
+
+}
+#endif
