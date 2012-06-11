f54e8fc27864fbffe1e07df54a9e2f284ad07c36
Juan Pablo Orsay <jporsay@gmail.com>
2012-06-11 11:32:19 -0300

    flipping image vertically since its loaded inverted

--- src/model/Texture.cpp
+++ src/model/Texture.cpp
@@ -11,6 +11,7 @@ void Texture::setSFImage(sf::Image* image) {
 	sf::Vector2u size = image->getSize();
 	this->height = size.y;
 	this->width = size.x;
+	image->flipVertically();
 	this->image = image;
 }
 unsigned int Texture::getWidth() {
