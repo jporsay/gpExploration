02068c369d6795797f8ff6fff0fe4a4ee5b9aa10
Juan Pablo Orsay <jporsay@gmail.com>
2012-06-11 01:01:47 -0300

    added material struct and renamed map to texCoord and vertex to vec3d

--- src/model/MeshStructs.h
+++ src/model/MeshStructs.h
@@ -2,22 +2,35 @@
 #define MESHSTRUCTS_H_
 
 #include <GL/glew.h>
+#include <string>
 
 typedef struct {
 	float x;
 	float y;
 	float z;
-} vertex;
+} vec3d;
 
 typedef struct {
-	GLushort a;
-	GLushort b;
-	GLushort c;
+	vec3d ambient;
+	vec3d diffuse;
+	vec3d specular;
+	float shininess;
+	float shininess_strength;
+	bool self_illum_flag;
+	float self_illum;
+	std::string textureFile;
+} material;
+
+typedef struct {
+	GLushort v1;
+	GLushort v2;
+	GLushort v3;
+	GLint material;
 } face;
 
 typedef struct {
 	float u;
 	float v;
-} mapCoord;
+} texCoord;
 
 #endif
