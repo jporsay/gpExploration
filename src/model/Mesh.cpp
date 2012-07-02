d4b9796729c3ebe36668e9c56738bdeea6512c33
Juan Pablo Orsay <jporsay@gmail.com>
2012-07-01 22:57:10 -0300

    also writing texture (though incorrectly)

--- src/model/Mesh.cpp
+++ src/model/Mesh.cpp
@@ -94,6 +94,8 @@ void Mesh::upload() {
 }
 
 void Mesh::draw(GLuint currProgram) {
+	GLuint textureUniform;
+	GLuint m_diffuseUniform;
 	if (this->ibo != 0) {
 		glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
 		glEnableVertexAttribArray(gl::util::GL_ATTRIB_VERTEX);
@@ -101,11 +103,17 @@ void Mesh::draw(GLuint currProgram) {
 		if (this->texVbo) {
 			glActiveTexture(GL_TEXTURE0);
 			glBindTexture(GL_TEXTURE_2D, this->textureId);
-			GLuint texUniform = glGetUniformLocation(currProgram, "tex_0");
-			glUniform1i(texUniform, 0);
+			textureUniform = glGetUniformLocation(currProgram, "tex_0");
+			glUniform1i(textureUniform, 0);
 			glBindBuffer(GL_ARRAY_BUFFER, this->texVbo);
 			glEnableVertexAttribArray(gl::util::GL_ATTRIB_TEXTURE);
 			glVertexAttribPointer(gl::util::GL_ATTRIB_TEXTURE, 2, GL_FLOAT, GL_FALSE, 0, 0);
+		}
+		if (this->materials.size() > 0) {
+			material* m = this->materials.at(0);
+			m_diffuseUniform = glGetUniformLocation(currProgram, "materialProperties.diffuseColor");
+//			LOG_INFO(utilities::toString(m->diffuse.x) + " " + utilities::toString(m->diffuse.y) + " " + utilities::toString(m->diffuse.z));
+			glUniform3f(m_diffuseUniform, m->diffuse.x, m->diffuse.y, m->diffuse.z);
 		}
 		GLint size;
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ibo);
